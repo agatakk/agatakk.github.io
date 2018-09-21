96b5c53218ce92706a2643320965300b15d88f7f
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2018-09-21 08:30:29 +0200

    Uporządkowano pliki

--- warsztat/JQUERY/js/1-document-ready.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-//wszystkie metody/funkcje itd siedzą w tej funkcji poniżej
-$(function () {
-
-    //    alert('test');
-    $('#first').css("color", "blue").hide(2000).show(3000);
-});
