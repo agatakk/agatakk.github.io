491f4b923f920f1929e02302f0fcbd6ef00f1bd5
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2019-02-28 15:56:10 +0100

    main.js - dodanie licznika

--- Samuraj-Projekty/Dodawanie-kwadratow/main.js
+++ Samuraj-Projekty/Dodawanie-kwadratow/main.js
@@ -2,12 +2,16 @@
 
 const btn = document.querySelector("button");
 
+let number = 1
+
 const showNumber = () => {
     const wrapper = document.querySelector(".inner_wrapper");
     const div = document.createElement('div');
     wrapper.appendChild(div);
-
-    div.textContent = "1";
+    
+    div.textContent = number;
+    number += 1; //licznik
+   
 
 }
 
