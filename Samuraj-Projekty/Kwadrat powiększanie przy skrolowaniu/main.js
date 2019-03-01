6347f95a988b30ceaf6eaa3af47ff70f62489fb3
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2019-03-01 12:47:13 +0100

    Nasłuchiwanie na scroll, stworzenie funkcji, flagi oraz warunków

--- Samuraj-Projekty/Kwadrat powiększanie przy skrolowaniu/main.js
+++ Samuraj-Projekty/Kwadrat powiększanie przy skrolowaniu/main.js
@@ -1,4 +1,27 @@
 const div = document.createElement("div");
 document.body.appendChild(div);
 
+let size = 100;
+div.style.height = size + "px";
+div.style.width = size + "px";
 
+let grow = true;
+
+const enlargeDiv = () => {
+    if(size>window.innerHeight / 2){
+        grow = !grow
+    }else if(size<=0){
+        grow = !grow
+    }
+    if(grow){
+        size += 6;
+        div.style.height = size + "px";
+        div.style.width = size + "px";
+    }else{
+        size -= 6;
+        div.style.height = size + "px";
+        div.style.width = size + "px";
+    }
+}
+
+window.addEventListener("scroll", enlargeDiv);
