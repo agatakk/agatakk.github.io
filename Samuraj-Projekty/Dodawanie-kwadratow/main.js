75e84e447dcc6afc25bdc025d7deecd3b5d1d1f8
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2019-03-01 09:07:38 +0100

    Dodanie warunku do funkcji showNumber - co piąty element inne ostylowanie

--- Samuraj-Projekty/Dodawanie-kwadratow/main.js
+++ Samuraj-Projekty/Dodawanie-kwadratow/main.js
@@ -4,6 +4,10 @@ const btn = document.querySelector("button");
 
 let number = 1
 
+if(number = 1){
+    console.log("true");
+}
+
 const showNumber = () => {
     const wrapper = document.querySelector(".inner_wrapper");
     const div = document.createElement('div');
@@ -11,6 +15,14 @@ const showNumber = () => {
     
     div.textContent = number;
     number += 1; //licznik
+
+    if(div.textContent % 5 == 0){
+        // div.style.borderRadius = "50%";
+        // div.style.backgroundColor = "rgb(214, 214, 214)";
+        // div.style.color = "rgb(194, 89, 120)";
+        // div.style.border = "1px solid rgb(194, 89, 120)";
+        div.classList.add("clicked-fifth");
+    }
    
 
 }
