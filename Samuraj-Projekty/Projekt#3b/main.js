08e6e8f58f51cfc266ea3d681a2beeb77de2c121
Agata Kubiak-Kokot <kubiak.aga@gmail.com>
2019-03-28 10:36:55 +0100

    Dodano rozwiązanie zadania

--- Samuraj-Projekty/Projekt#3b/main.js
+++ Samuraj-Projekty/Projekt#3b/main.js
@@ -1,29 +1,33 @@
 let size = 10;
-let orderElement = 0;
+let orderElement = 1;
 
 const init = () => {
  const btn = document.createElement("button");
  btn.textContent = "Dodaj 10 elementów do listy"
  document.body.appendChild(btn);
-
-
- 
-
+ const btnReset = document.createElement("button");
+ btnReset.textContent = "Wyczyść";
+ document.body.appendChild(btnReset);
+ const ul = document.createElement("ul");
+ document.body.appendChild(ul);
  btn.addEventListener("click", createLiElements);
+ btnReset.addEventListener("click", clearList);
 }
 
 const createLiElements = () => {
-    const ul = document.createElement("ul");
-    document.body.appendChild(ul);
+    for(let i = 0; i< 10; i++){
     const li = document.createElement("li");
-    ++orderElement
-    size +=size
-    for(let i = 0; i<size; i++){
-    
-    li.textContent = `element-${orderElement}`;
-    
+    document.querySelector('ul').appendChild(li);
+    li.textContent = `element-${orderElement++}`;
+    li.style.fontSize = `${size++}px`;
 }
-ul.appendChild(li)*size;
+
+}
+
+const clearList = ()=>{
+    document.querySelector('ul').textContent = "";
+    size = 10;
+    orderElement = 1;
 }
 
 init()
